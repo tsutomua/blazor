ca90b44204205d0b1beea9460f905dcefc729e02
tsutomua <tsutomua@hotmail.com>
2025-01-08 09:31:40 -0800

    Added images.

--- BlazorLocalizationTest/BlazorLocalizationTest/Utils/Constants.cs
+++ BlazorLocalizationTest/BlazorLocalizationTest/Utils/Constants.cs
@@ -4,6 +4,8 @@ namespace Utils
     {
         public const string SupportedCultureCodes = "SupportedCultureCodes";
         public const string homePage = "homePage";
+        public const string mainImagePath = "mainImagePath";
+        public const string mainImageAltText = "mainImageAltText";
         public const string counter = "counter";
     }
 }
